ce4ea10873d4158ad46aa2b51d1c9a249cbf5ab2
ace2102 <ace2102@gmail.com>
2026-05-29 17:38:41 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'rp-v2';
+const FILES = [
+  '/Retirement-Planner/',
+  '/Retirement-Planner/index.html',
+  '/Retirement-Planner/manifest.json',
+  '/Retirement-Planner/icon-192.png',
+  '/Retirement-Planner/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(() => caches.match('/Retirement-Planner/index.html'))
+  );
+});
